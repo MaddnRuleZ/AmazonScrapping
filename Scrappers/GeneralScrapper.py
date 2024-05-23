158a0975b503d90b1e2165c8e742e0b6737fab4b
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2024-05-23 15:15:11 +0200

    Hotfix?

--- Scrappers/GeneralScrapper.py
+++ Scrappers/GeneralScrapper.py
@@ -26,7 +26,7 @@ class GeneralScrapper:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--remote-debugging-port=9222')  #
+        chrome_options.add_argument('--remote-debugging-port=9515')  #
         chrome_options.add_argument('--disable-gpu')
 
         # chrome_options.add_argument("user-data-dir=C:/Users/xmadd/Desktop/ChromeSeleniumStorage")
