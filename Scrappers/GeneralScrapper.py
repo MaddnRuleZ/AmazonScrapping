d5974d9aaf344b14a2e33ee863a7321d32d8de02
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2024-05-23 15:10:41 +0200

    Hotfix?

--- Scrappers/GeneralScrapper.py
+++ Scrappers/GeneralScrapper.py
@@ -23,19 +23,18 @@ class GeneralScrapper:
         print("Innit Driver")
         # HEADLESS OPTION
         chrome_options = Options()
-
-        # todo EINSTELLUNG FÜR BROWSER SICHTBAR
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         # chrome_options.add_argument("user-data-dir=C:/Users/xmadd/Desktop/ChromeSeleniumStorage")
+        chrome_options.binary_location = "/usr/local/bin/chromedriver"
 
-        chrome_options.add_argument("--no-sandbox")
+
+        '''chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36")
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.binary_location = "/usr/local/bin/chromedriver"
+        chrome_options.add_argument("--start-maximized")'''
 
         try:
             self.driver = webdriver.Chrome(options=chrome_options)
